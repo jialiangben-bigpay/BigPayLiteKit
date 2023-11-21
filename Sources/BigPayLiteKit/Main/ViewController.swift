4a996376557863655d2b0e6aa3c90cab56906678
jialiangben-bigpay <benjamin.lee@bigpayme.com>
2023-11-21 15:49:09 +0800

    Add Pop Func

--- Sources/BigPayLiteKit/Main/ViewController.swift
+++ Sources/BigPayLiteKit/Main/ViewController.swift
@@ -11,10 +11,32 @@ import shared
 class ViewController: UIViewController {
     @IBOutlet weak var balanceAmount: UILabel!
     
+    @IBOutlet weak var settingIcon: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         balanceAmount.text = Greeting().getUserProfile().amount
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        settingIcon.isUserInteractionEnabled = true
+        settingIcon.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        self.navigationController?.popViewController(animated: true)
     }
 
 }
