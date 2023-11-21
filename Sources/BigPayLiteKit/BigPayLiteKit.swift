461acb71fb36e85ab0773634815829fc2095a3dd
jialiangben-bigpay <benjamin.lee@bigpayme.com>
2023-11-21 15:27:26 +0800

    Rename Storybaord

--- Sources/BigPayLiteKit/BigPayLiteKit.swift
+++ Sources/BigPayLiteKit/BigPayLiteKit.swift
@@ -4,7 +4,7 @@ import UIKit
 
 public class BigPayLiteKit {
     public static func showBigPayLite(_ navigationController: UINavigationController) {
-        let destinationVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "VC") as! ViewController
+        let destinationVC = UIStoryboard(name: "BigPayMain", bundle: nil).instantiateViewController(withIdentifier: "VC") as! ViewController
         navigationController.pushViewController(destinationVC, animated: true)
     }
 }
