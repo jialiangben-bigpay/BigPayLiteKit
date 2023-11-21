adfecdc86b9f5c90863ab7b7d7da588d6aa52ec6
jialiangben-bigpay <benjamin.lee@bigpayme.com>
2023-11-21 15:22:34 +0800

    Change to static func

--- Sources/BigPayLiteKit/BigPayLiteKit.swift
+++ Sources/BigPayLiteKit/BigPayLiteKit.swift
@@ -3,7 +3,7 @@ import Foundation
 import UIKit
 
 public class BigPayLiteKit {
-    public func showBigPayLite(_ navigationController: UINavigationController) {
+    public static func showBigPayLite(_ navigationController: UINavigationController) {
         let destinationVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as! ViewController
         navigationController.pushViewController(destinationVC, animated: true)
     }
